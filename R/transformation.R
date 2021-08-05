e998fa935de1cd7d98f9454168ae6e6a2ccc5695
Gufeng Zhou <gufengzhou@fb.com>
2021-08-05 17:28:13 +0200

    replace grid.arrange by wrap_plots

--- R/transformation.R
+++ R/transformation.R
@@ -25,7 +25,7 @@ mic_men <- function(x, Vmax, Km, reverse = FALSE) {
 }
 
 ####################################################################
-#' Geometric adstocking function 
+#' Geometric adstocking function
 #'
 #' Describe function.
 #'
@@ -37,10 +37,10 @@ adstock_geometric <- function(x, theta) {
   for (xi in 2:length(x_decayed)) {
     x_decayed[xi] <- x[xi] + theta * x_decayed[xi-1]
   }
-  
+
   thetaVecCum <- theta
   for (t in 2:length(x)) {thetaVecCum[t] <- thetaVecCum[t-1]*theta} # plot(thetaVecCum)
-  
+
   return(list(x_decayed=x_decayed, thetaVecCum = thetaVecCum))
 }
 
@@ -59,7 +59,7 @@ adstock_weibull <- function(x, shape , scale) {
   scaleTrans = round(quantile(x_bin, scale),0) #
   thetaVec <- c(1, 1-pweibull(head(x_bin, -1), shape = shape, scale = scaleTrans)) # plot(thetaVec)
   thetaVecCum <- cumprod(thetaVec)  # plot(thetaVecCum)
-  
+
   x_decayed <- mapply(function(x, y) {
     x.vec <- c(rep(0,y-1), rep(x, x.n-y+1))
     thetaVecCumLag <- shift(thetaVecCum, y-1, fill = 0)
@@ -68,7 +68,7 @@ adstock_weibull <- function(x, shape , scale) {
     return(x.prod)
   }, x=x , y=x_bin)
   x_decayed <- rowSums(x_decayed)
-  
+
   return(list(x_decayed=x_decayed, thetaVecCum = thetaVecCum))
 }
 
@@ -83,9 +83,9 @@ adstock_weibull <- function(x, shape , scale) {
 #' @param x_marginal xxxxxx
 
 saturation_hill <- function(x, alpha, gamma, x_marginal = NULL) {
-  
+
   gammaTrans <- round(quantile(seq(range(x)[1], range(x)[2], length.out = 100), gamma),4)
-  
+
   if (is.null(x_marginal)) {
     x_scurve <-  x**alpha / (x**alpha + gammaTrans**alpha) # plot(x_scurve) summary(x_scurve)
   } else {
@@ -110,7 +110,7 @@ plot_adstock <- function(plotAdstockCurves) {
     shapeVec <- c(2, 2, 2, 2, 2, 2, 0.01, 0.1, 0.5, 1, 1.5, 2)
     scaleVec <- c(0.01, 0.05, 0.1, 0.15, 0.2, 0.5, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05)
     paramRotate <- c(rep("scale",6), rep("shape",6))
-    
+
     for (v in 1:length(shapeVec)) {
       dt_weibull<- data.table(x=1:100,
                               decay_accumulated=adstock_weibull(rep(1, 100), shape=shapeVec[v], scale=scaleVec[v])$thetaVecCum
@@ -118,35 +118,35 @@ plot_adstock <- function(plotAdstockCurves) {
                               ,scale=scaleVec[v]
                               ,param=paramRotate[v])
       dt_weibull[, halflife:= which.min(abs(decay_accumulated -0.5))]
-      
+
       weibullCollect[[v]] <- dt_weibull
     }
-    
+
     weibullCollect <- rbindlist(weibullCollect)
     #weibullCollect[, ':='(shape=as.factor(shape), scale=as.factor(scale))]
     weibullCollect[, scale_shape_halflife:=paste(scale, shape,halflife, sep = "_")]
-    p1 <- ggplot(weibullCollect[param=="scale"], aes(x=x, y=decay_accumulated)) + 
+    p1 <- ggplot(weibullCollect[param=="scale"], aes(x=x, y=decay_accumulated)) +
       geom_line(aes(color=scale_shape_halflife)) +
       geom_hline(yintercept=0.5, linetype="dashed", color = "gray") +
       geom_text(aes(x = max(x), y = 0.5, vjust = -0.5, hjust= 1, label = "Halflife = time until effect reduces to 50%"), colour="gray") +
-      labs(title="Weibull adstock transformation - scale changes", 
+      labs(title="Weibull adstock transformation - scale changes",
            subtitle="Halflife = time until effect reduces to 50%",
            x="time unit",
-           y="Media decay accumulated") 
-    p2 <- ggplot(weibullCollect[param=="shape"], aes(x=x, y=decay_accumulated)) + 
+           y="Media decay accumulated")
+    p2 <- ggplot(weibullCollect[param=="shape"], aes(x=x, y=decay_accumulated)) +
       geom_line(aes(color=scale_shape_halflife)) +
       geom_hline(yintercept=0.5, linetype="dashed", color = "gray") +
       geom_text(aes(x = max(x), y = 0.5, vjust = -0.5, hjust= 1, label = "Halflife = time until effect reduces to 50%"), colour="gray") +
-      labs(title="Weibull adstock transformation - shape changes", 
+      labs(title="Weibull adstock transformation - shape changes",
            subtitle="Halflife = time until effect reduces to 50%",
            x="time unit",
-           y="Media decay accumulated") 
-    
+           y="Media decay accumulated")
+
     ## plot geometric
-    
+
     geomCollect <- list()
     thetaVec <- c(0.01, 0.05, 0.1, 0.2, 0.5, 0.6, 0.7, 0.8, 0.9)
-    
+
     for (v in 1:length(thetaVec)) {
       thetaVecCum <- 1
       for (t in 2:100) {thetaVecCum[t] <- thetaVecCum[t-1]*thetaVec[v]}
@@ -158,20 +158,22 @@ plot_adstock <- function(plotAdstockCurves) {
     }
     geomCollect <- rbindlist(geomCollect)
     geomCollect[, theta_halflife:=paste(theta,halflife, sep = "_")]
-    
-    p3 <- ggplot(geomCollect, aes(x=x, y=decay_accumulated)) + 
+
+    p3 <- ggplot(geomCollect, aes(x=x, y=decay_accumulated)) +
       geom_line(aes(color=theta_halflife)) +
       geom_hline(yintercept=0.5, linetype="dashed", color = "gray") +
       geom_text(aes(x = max(x), y = 0.5, vjust = -0.5, hjust= 1, label = "Halflife = time until effect reduces to 50%"), colour="gray") +
-      labs(title="Geometric adstock transformation - theta changes", 
+      labs(title="Geometric adstock transformation - theta changes",
            subtitle="Halflife = time until effect reduces to 50%",
            x="time unit",
-           y="Media decay accumulated") 
+           y="Media decay accumulated")
     #print(p3)
-    
-    grid.arrange(p1,p2, p3, layout_matrix = rbind(c(3,1),
-                                                  c(3,2)))
-    
+
+    return(wrap_plots(A=p1, B=p2, C=p3, design = "AC
+               BC"))
+    # grid.arrange(p1,p2, p3, layout_matrix = rbind(c(3,1),
+    #                                               c(3,2)))
+
   }
 }
 
@@ -189,7 +191,7 @@ plot_saturation <- function(plotResponseCurves) {
     xSample <- 1:100
     alphaSamp <- c(0.1, 0.5, 1, 2, 3)
     gammaSamp <- c(0.1, 0.3, 0.5, 0.7, 0.9)
-    
+
     ## plot alphas
     hillAlphaCollect <- list()
     for (i in 1:length(alphaSamp)) {
@@ -199,11 +201,11 @@ plot_saturation <- function(plotResponseCurves) {
     }
     hillAlphaCollect <- rbindlist(hillAlphaCollect)
     hillAlphaCollect[, alpha:=as.factor(alpha)]
-    p1 <- ggplot(hillAlphaCollect, aes(x=x, y=y, color=alpha)) + 
+    p1 <- ggplot(hillAlphaCollect, aes(x=x, y=y, color=alpha)) +
       geom_line() +
       labs(title = "Cost response with hill function"
            ,subtitle = "Alpha changes while gamma = 0.5")
-    
+
     ## plot gammas
     hillGammaCollect <- list()
     for (i in 1:length(gammaSamp)) {
@@ -213,11 +215,11 @@ plot_saturation <- function(plotResponseCurves) {
     }
     hillGammaCollect <- rbindlist(hillGammaCollect)
     hillGammaCollect[, gamma:=as.factor(gamma)]
-    p2 <- ggplot(hillGammaCollect, aes(x=x, y=y, color=gamma)) + 
+    p2 <- ggplot(hillGammaCollect, aes(x=x, y=y, color=gamma)) +
       geom_line() +
       labs(title = "Cost response with hill function"
            ,subtitle = "Gamma changes while alpha = 2")
-    
-    grid.arrange(p1,p2, nrow=1)
+
+    return(p1+p2)
   }
 }
