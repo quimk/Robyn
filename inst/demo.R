4b54226303da8c8d807493f378b43c47bb2661f2
Gufeng Zhou <gufengzhou@fb.com>
2021-08-20 11:42:31 +0200

    demo.R adaption

--- inst/demo.R
+++ inst/demo.R
@@ -58,6 +58,8 @@ robyn_object <- "~/Desktop/Robyn.RData"
 #### Step 2a: For first time user: Model specification in 4 steps
 
 #### 2a-1: First, specify input data & model parameters
+
+# Run ?robyn_inputs to check parameter definition
 InputCollect <- robyn_inputs(
   dt_input = dt_simulated_weekly
   ,dt_holidays = dt_prophet_holidays
@@ -99,7 +101,7 @@ InputCollect <- robyn_inputs(
   ### set model parameters
 
   ## set cores for parallel computing
-  ,cores = 6 # I am using 6 cores from 8 on my local machine. Use availableCores() to find out cores
+  ,cores = 8 # I am using 6 cores from 8 on my local machine. Use availableCores() to find out cores
 
   ## set rolling window start
   ,window_start = "2016-11-23"
@@ -108,11 +110,11 @@ InputCollect <- robyn_inputs(
   ## set model core features
   ,adstock = "geometric" # geometric or weibull. weibull is more flexible, yet has one more
   # parameter and thus takes longer
-  ,iterations = 100  # number of allowed iterations per trial. 2000 is recommended
+  ,iterations = 2000  # number of allowed iterations per trial. 2000 is recommended
 
   ,nevergrad_algo = "TwoPointsDE" # recommended algorithm for Nevergrad, the gradient-free
   # optimisation library https://facebookresearch.github.io/nevergrad/index.html
-  ,trials = 2 # number of allowed iterations per trial. 5 is recommended without calibration,
+  ,trials = 5 # number of allowed iterations per trial. 5 is recommended without calibration,
   # 10 with calibration.
 
   # Time estimation: with geometric adstock, 2000 iterations * 5 trials
@@ -160,6 +162,7 @@ plot_saturation(FALSE) # s-curve transformation example plot,
 ## 3. set each hyperparameter bounds. They either contains two values e.g. c(0, 0.5),
 # or only one value (in which case you've "fixed" that hyperparameter)
 
+# Run ?hyper_names to check parameter definition
 hyper_names(adstock = InputCollect$adstock, all_media = InputCollect$all_media)
 
 hyperparameters <- list(
@@ -207,19 +210,19 @@ InputCollect <- robyn_inputs(InputCollect = InputCollect, hyperparameters = hype
 
 #### 2a-4: Fourth (optional), model calibration / add experimental input
 
-dt_calibration <- data.frame(
-  channel = c("facebook_I",  "tv_S", "facebook_I")
-  # channel name must in paid_media_vars
-  , liftStartDate = as.Date(c("2018-05-01", "2017-11-27", "2018-07-01"))
-  # liftStartDate must be within input data range
-  , liftEndDate = as.Date(c("2018-06-10", "2017-12-03", "2018-07-20"))
-  # liftEndDate must be within input data range
-  , liftAbs = c(400000, 300000, 200000) # Provided value must be
-  # tested on same campaign level in model and same metric as dep_var_type
-)
-
-InputCollect <- robyn_inputs(InputCollect = InputCollect
-                             , calibration_input = dt_calibration)
+# dt_calibration <- data.frame(
+#   channel = c("facebook_I",  "tv_S", "facebook_I")
+#   # channel name must in paid_media_vars
+#   , liftStartDate = as.Date(c("2018-05-01", "2017-11-27", "2018-07-01"))
+#   # liftStartDate must be within input data range
+#   , liftEndDate = as.Date(c("2018-06-10", "2017-12-03", "2018-07-20"))
+#   # liftEndDate must be within input data range
+#   , liftAbs = c(400000, 300000, 200000) # Provided value must be
+#   # tested on same campaign level in model and same metric as dep_var_type
+# )
+#
+# InputCollect <- robyn_inputs(InputCollect = InputCollect
+#                              , calibration_input = dt_calibration)
 
 
 ################################################################
@@ -230,79 +233,39 @@ InputCollect <- robyn_inputs(InputCollect = InputCollect
 # InputCollect <- robyn_inputs(
 #   dt_input = dt_simulated_weekly
 #   ,dt_holidays = dt_prophet_holidays
-#
-#   ### set variables
-#
-#   ,date_var = "DATE" # date format must be "2020-01-01"
-#   ,dep_var = "revenue" # there should be only one dependent variable
-#   ,dep_var_type = "revenue" # "revenue" or "conversion"
-#
-#   ,prophet_vars = c("trend", "season", "holiday") # "trend","season", "weekday", "holiday"
-#   # are provided and case-sensitive. Recommended to at least keep Trend & Holidays
-#   ,prophet_signs = c("default","default", "default") # c("default", "positive", and "negative").
-#   # Recommend as default.Must be same length as prophet_vars
-#   ,prophet_country = "DE"# only one country allowed once. Including national holidays
-#   # for 59 countries, whose list can be found on our githut guide
-#
-#   ,context_vars = c("competitor_sales_B", "events") # typically competitors, price &
-#   # promotion, temperature, unemployment rate etc
-#   ,context_signs = c("default", "default") # c("default", " positive", and "negative"),
-#   # control the signs of coefficients for baseline variables
-#
-#   ,paid_media_vars = c("tv_S", "ooh_S"	,	"print_S"	,"facebook_I" ,"search_clicks_P")
-#   # c("tv_S"	,"ooh_S",	"print_S"	,"facebook_I", "facebook_S","search_clicks_P"	,"search_S")
-#   # we recommend to use media exposure metrics like impressions, GRP etc for the model.
-#   # If not applicable, use spend instead
-#   ,paid_media_signs = c("positive", "positive","positive", "positive", "positive")
-#   # c("default", "positive", and "negative"). must have same length as paid_media_vars.
-#   # Controls the signs of coefficients for media variables
-#   ,paid_media_spends = c("tv_S","ooh_S",	"print_S"	,"facebook_S", "search_S")
-#   # spends must have same order and same length as paid_media_vars
-#
+#   ,date_var = "DATE"
+#   ,dep_var = "revenue"
+#   ,dep_var_type = "revenue"
+#   ,prophet_vars = c("trend", "season", "holiday")
+#   ,prophet_signs = c("default","default", "default")
+#   ,prophet_country = "DE"
+#   ,context_vars = c("competitor_sales_B", "events")
+#   ,context_signs = c("default", "default")
+#   ,paid_media_vars = c("tv_S", "ooh_S", 	"print_S", "facebook_I", "search_clicks_P")
+#   ,paid_media_signs = c("positive", "positive", "positive", "positive", "positive")
+#   ,paid_media_spends = c("tv_S", "ooh_S",	"print_S", "facebook_S", "search_S")
 #   ,organic_vars = c("newsletter")
-#   ,organic_signs = c("positive") # must have same length as organic_vars
-#
-#   ,factor_vars = c("events") # specify which variables in context_vars and
-#   # organic_vars are factorial
-#
-#   ### set model parameters
-#
-#   ## set cores for parallel computing
-#   ,cores = 6 # I am using 6 cores from 8 on my local machine. Use availableCores() to find out cores
-#
-#   ## set rolling window start
+#   ,organic_signs = c("positive")
+#   ,factor_vars = c("events")
+#   ,cores = 6
 #   ,window_start = "2016-11-23"
 #   ,window_end = "2018-08-22"
-#
-#   ## set model core features
-#   ,adstock = "geometric" # geometric or weibull. weibull is more flexible, yet has one more
-#   # parameter and thus takes longer
-#   ,iterations = 100  # number of allowed iterations per trial. 2000 is recommended
-#
-#   ,nevergrad_algo = "TwoPointsDE" # recommended algorithm for Nevergrad, the gradient-free
-#   # optimisation library https://facebookresearch.github.io/nevergrad/index.html
-#   ,trials = 2 # number of allowed iterations per trial. 5 is recommended without calibration,
-#   # 10 with calibration.
-#
+#   ,adstock = "geometric"
+#   ,iterations = 2000
+#   ,trials = 5
 #   ,hyperparameters = hyperparameters # as in 2a-2 above
-#
-#   ,calibration_input = dt_calibration # as in 2a-4 above
-#
-#   # Time estimation: with geometric adstock, 2000 iterations * 5 trials
-#   # and 6 cores, it takes less than 1 hour. Weibull takes at least twice as much time.
+#   #,calibration_input = dt_calibration # as in 2a-4 above
 # )
 
 ################################################################
 #### Step 3: Build initial model
 
+# Run ?robyn_run to check parameter definition
 OutputCollect <- robyn_run(
   InputCollect = InputCollect # feed in all model specification
-  , plot_folder = robyn_object # plots will be saved in the same folder as robyn_object.
-  # Other paths are also possible
-  , pareto_fronts = 1 # How many Pareto fronts to be exported as model output. The higher,
-  # the more model results/ one-pagers provided
-  , plot_pareto = TRUE # Disabling plat_pareto speeds up processing time in the end,
-  # but plots won't be saved
+  , plot_folder = robyn_object # plots will be saved in the same folder as robyn_object
+  , pareto_fronts = 1
+  , plot_pareto = TRUE
   )
 
 ## Besides one-pager plots: there are 4 csv output saved in the folder for further usage
@@ -319,7 +282,7 @@ OutputCollect <- robyn_run(
 ## your business reality
 
 OutputCollect$allSolutions # get all model IDs in result
-select_model <- OutputCollect$allSolutions[1] # select one from above (first as example)
+select_model <- "3_206_1" # select one from above (first as example)
 robyn_save(robyn_object = robyn_object # model object location and name
            , select_model = select_model # selected model ID
            , InputCollect = InputCollect # all model input
@@ -333,30 +296,43 @@ robyn_save(robyn_object = robyn_object # model object location and name
 ## Budget allocator result requires further validation. Please use this result with caution.
 ## Don't interpret budget allocation result if selected result doesn't meet business expectation
 
-# check media summary for selected model
+# Check media summary for selected model
 OutputCollect$xDecompAgg[solID == select_model & !is.na(mean_spend)
                          , .(rn, coef,mean_spend, mean_response, roi_mean
                              , total_spend, total_response=xDecompAgg, roi_total, solID)]
 
+# Run ?robyn_allocator to check parameter definition
+# Run the "max_historical_response" scenario: "What's the revenue lift potential with the
+# same historical spend level and what is the spend mix?"
 AllocatorCollect <- robyn_allocator(
-
   InputCollect = InputCollect
   , OutputCollect = OutputCollect
-  , select_model = select_model # input one of the model IDs in OutputCollect$allSolutions
-  #to get optimisation result
-  , scenario = "max_historical_response" # options are "max_historical_response"
-  #, "max_response_expected_spend"
-  , channel_constr_low = c(0.7, 0.7, 0.7, 0.7, 0.7) # must be between 0.01-1 and has same
-  #length and order as paid_media_vars
-  , channel_constr_up = c(1.2, 1.5, 1.5, 1.5, 1.5) # not recommended to 'exaggerate' upper
-  #bounds. 1.5 means channel budget can increase to 150% of current level
-
-  # , expected_spend = 100000 # specify future spend volume. only applies when scenario =
-  # # "max_response_expected_spend"
-  # , expected_spend_days = 90 # specify period for the future spend volumne in days.
-  # # only applies when scenario = "max_response_expected_spend"
-  # , optim_algo = "SLSQP_AUGLAG" # options are "MMA_AUGLAG", "SLSQP_AUGLAG"
+  , select_model = select_model
+  , scenario = "max_historical_response"
+  , channel_constr_low = c(0.7, 0.7, 0.7, 0.7, 0.7)
+  , channel_constr_up = c(1.2, 1.5, 1.5, 1.5, 1.5)
 )
+
+# View allocator result. Last column "optmResponseUnitTotalLift" is the total response lift.
+AllocatorCollect$dt_optimOut
+
+# Run the "max_response_expected_spend" scenario: "What's the maximum response for a given
+# total spend based on historical saturation and what is the spend mix?" "optmSpendShareUnit"
+# is the optimum spend share.
+AllocatorCollect <- robyn_allocator(
+  InputCollect = InputCollect
+  , OutputCollect = OutputCollect
+  , select_model = select_model
+  , scenario = "max_response_expected_spend"
+  , channel_constr_low = c(0.7, 0.7, 0.7, 0.7, 0.7)
+  , channel_constr_up = c(1.2, 1.5, 1.5, 1.5, 1.5)
+  , expected_spend = 1000000 # Total spend to be simulated
+  , expected_spend_days = 7 # Duration of expected_spend in days
+)
+
+# View allocator result. Column "optmResponseUnitTotal" is the maximum unit (weekly with
+# simulated dataset) response. "optmSpendShareUnit" is the optimum spend share.
+AllocatorCollect$dt_optimOut
 
 ## QA optimal response
 # select_media <- "search_clicks_P"
@@ -381,18 +357,19 @@ AllocatorCollect <- robyn_allocator(
 ## it might be better to rebuild the model
 ## 2, new variables are added
 
+# Run ?robyn_refresh to check parameter definition
 Robyn <- robyn_refresh(
-  robyn_object = robyn_object # the location of your Robyn.RData object
+  robyn_object = robyn_object
   , dt_input = dt_simulated_weekly
   , dt_holidays = dt_prophet_holidays
-  , refresh_steps = 13 # refresh_steps = 4 means refresh model's rolling window will move
-  #forward 4 weeks
-  , refresh_mode = "auto" # "auto"  or "manual". auto means the refresh function will move
-  #forward until no more data available
-  , refresh_iters = 200 # Iteration for refresh. 200 is rough estimation. We'll still
-  #figuring out what's the ideal number.
-  , refresh_trials = 3 # trial for refresh
+  , refresh_steps = 4
+  , refresh_mode = "manual"
+  , refresh_iters = 100 # Iteration for refresh. 600 is rough estimation. We'll still
+  # figuring out what's the ideal number.
+  , refresh_trials = 3
 )
+
+AllocatorCollect$dt_optimOut
 
 ## Besides plots: there're 4 csv output saved in the folder for further usage
 # report_hyperparameters.csv, hyperparameters of all selected model for reporting
@@ -401,24 +378,19 @@ Robyn <- robyn_refresh(
 # report_alldecomp_matrix.csv,all decomposition vectors of independent variables
 
 
+load(robyn_object)
+names(Robyn)
+
 ################################################################
 #### Step 7: Get budget allocation recommendation based on selected refresh runs
 
+# Run ?robyn_allocator to check parameter definition
 AllocatorCollect <- robyn_allocator(
   robyn_object = robyn_object
-  #, select_build = 0 # 0 is initial model, 1 is first refresh etc. Default to the latest refresh
-  , scenario = "max_historical_response" # "max_historical_response"
-  #or "max_response_expected_spend"
-  , channel_constr_low = c(0.7, 0.7, 0.7, 0.7, 0.7) # must be between 0.01-1 and has
-  #same length and order as paid_media_vars
-  , channel_constr_up = c(1.2, 1.5, 1.5, 1.5, 1.5) # not recommended to 'exaggerate' upper
-  #bounds. 1.5 means channel budget can increase to 150% of current level
-
-  # , expected_spend = 100000 # specify future spend volume. only applies when scenario =
-  # #"max_response_expected_spend"
-  # , expected_spend_days = 90 # specify period for the future spend volumne in days.
-  # #only applies when scenario = "max_response_expected_spend"
-  # , optim_algo = "SLSQP_AUGLAG" # options are "MMA_AUGLAG", "SLSQP_AUGLAG"
+  , select_build = 2 # Use third refresh model
+  , scenario = "max_historical_response"
+  , channel_constr_low = c(0.7, 0.7, 0.7, 0.7, 0.7)
+  , channel_constr_up = c(1.2, 1.5, 1.5, 1.5, 1.5)
 )
 
 
@@ -426,6 +398,8 @@ AllocatorCollect <- robyn_allocator(
 #### Step 8: get marginal returns
 
 ## Example of how to get marginal ROI of next 1000$ from the 80k spend level for search channel
+
+# Run ?robyn_response to check parameter definition
 
 # Get response for 80k
 Spend1 <- 80000
